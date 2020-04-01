1ff89fb697bb69ef3d8b44e82dcd9947ba2c64b1
TaufiqTab <taufiqtab77@gmail.com>
2020-04-01 14:01:17 +0700

    My First LiveWare

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>HELLO : MY FIRST LIVEWARE APPS</h1>
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h1>{{ $count }}</h1>
+        <button wire:click="decrement">-</button>
+    </div>
+</div>
